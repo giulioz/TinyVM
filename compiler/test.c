7c6ab9fb31b50cdd47c4c1352a195ec62bd80985
giulioz <mail.zausa.giulio@gmail.com>
2019-05-17 15:11:22 +0200

    Test realistic

--- compiler/test.c
+++ compiler/test.c
@@ -1,9 +1,10 @@
 int fattoriale(int n) {
   int i;
   int risultato = 1;
-  for (i = 2; i <= n; i = i + 1) {
+  for (i = 2; i < n; i = i + 1) {
     risultato = risultato * i;
   }
+  risultato = risultato * i;
   return risultato;
 }
 
@@ -11,25 +12,4 @@ int main() {
   int r = fattoriale(5);
   print(r);
   return 0;
-}
-
-int fn() {
-  int a = 3;
-  int b = 2;
-
-  if (a == 2) {
-    return 5;
-  } else {
-    return 2;
-  }
-
-  for (b = 0; b < 5; b = b + 1) {
-    a = a * 2;
-  }
-
-  while (a == 0) {
-    a = b + 4 - fn(3 + a);
-  }
-
-  return a;
 }
