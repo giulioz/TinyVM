f80929a3a8c1ed8d4303ba169d0d9ee2857e0970
giulioz <mail.zausa.giulio@gmail.com>
2019-05-18 15:05:53 +0200

    Now deploy

--- assembler/dist/web.js
+++ assembler/dist/web.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56736" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57863" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -388,4 +388,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/web.js.map
+//# sourceMappingURL=web.js.map
